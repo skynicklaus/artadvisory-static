ca9ea19823d078a407ce6591f69e328d1e9d13dd
skynicklaus <sky.nicklaus@gmail.com>
2023-03-18 03:40:08 +0800

    apple device bug fix

--- src/routes/(app)/HeroLanding.svelte
+++ src/routes/(app)/HeroLanding.svelte
@@ -6,8 +6,8 @@
 	import CarouselSmall from './CarouselSmall.svelte';
 </script>
 
-<div class="container mx-auto px-4 pt-40 lg:px-12 lg:pt-48 lg:pb-10 xl:pt-52 2xl:pt-64">
-	<div class="grid-template lg:grid xl:grid-cols-2">
+<div class="container px-4 pt-40 mx-auto lg:px-12 lg:pt-48 lg:pb-10 xl:pt-52 2xl:pt-64">
+	<div class="lg:grid xl:grid-cols-2 grid-template">
 		<div class="flex flex-col gap-4 xl:mt-16">
 			<div
 				class="text-3xl font-bold text-gray-50 sm:mx-auto sm:max-w-sm sm:text-center lg:mx-0 lg:text-left xl:w-3/5 xl:max-w-md"
@@ -15,32 +15,32 @@
 				<h1>Join our revolutionary fine art painting investment platform.</h1>
 			</div>
 			<div class="text-gray-50 sm:mx-auto lg:mx-0">
-				<div class="flex items-center gap-2 pb-4 lg:text-base">
+				<div class="flex gap-2 items-center pb-4 lg:text-base">
 					<div class="w-4 xl:w-7">
 						<Star />
 					</div>
 					<p>Invest in highly-prized paintings</p>
 				</div>
-				<div class="flex items-center gap-2 pb-4 xl:text-base">
+				<div class="flex gap-2 items-center pb-4 xl:text-base">
 					<div class="w-4 xl:w-7">
 						<Star />
 					</div>
 					<p>Attractive historical returns</p>
 				</div>
-				<div class="flex items-center gap-2 pb-4 xl:text-base">
+				<div class="flex gap-2 items-center pb-4 xl:text-base">
 					<div class="w-4 xl:w-7">
 						<Star />
 					</div>
 					<p>Get monthly benefits upon investing</p>
 				</div>
-				<div class="flex items-center gap-2 pb-4 xl:text-base">
+				<div class="flex gap-2 items-center pb-4 xl:text-base">
 					<div class="w-4 xl:w-7">
 						<Star />
 					</div>
 					<p>Receive proceeds upon painting sold</p>
 				</div>
 			</div>
-			<div class="mx-auto mb-6 flex w-full">
+			<div class="flex mx-auto mb-6 w-full">
 				<Button
 					href="/contact"
 					title="Request Invitation"
@@ -51,11 +51,11 @@
 			</div>
 		</div>
 		<div
-			class="relative mt-14 hidden w-full overflow-hidden sm:flex sm:items-start sm:px-8 lg:mt-0"
+			class="hidden overflow-hidden relative mt-14 w-full sm:grid sm:grid-cols-1 sm:px-8 lg:mt-0"
 		>
 			<Carousel images={shopData} />
 		</div>
-		<div class="relative overflow-hidden sm:hidden">
+		<div class="overflow-hidden relative sm:hidden">
 			<CarouselSmall images={shopData} />
 		</div>
 	</div>
